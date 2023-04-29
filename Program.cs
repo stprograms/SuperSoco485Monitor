76f37e7e8ab9a449375f6fb8be522a281767405f
Kurzmann, Andreas <andreas.kurzmann@funkwerk.com>
2023-04-29 16:15:25 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using NLog;
+using NLog.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+
+var sec = config.GetSection("NLog");
+LogManager.Configuration = new NLogLoggingConfiguration(sec);
+NLog.Logger log = LogManager.GetCurrentClassLogger();
+
+var cfg = config.GetSection("Monitor");
+
+log.Info($"Starting Serial Monitor on port {cfg["port"]}");
+
+string port;
+uint? delayMS = null;
+
+if (cfg == null)
+{
+    log.Error("Missing configuration");
+    return;
+}
+
+if (cfg["port"] == null)
+{
+    log.Error("ComPort is not specified");
+    return;
+}
+else
+{
+    port = cfg["port"]!;
+}
+
+if (cfg["delayMS"] != null)
+{
+    delayMS = uint.Parse(cfg["delayMS"]!);
+}
+
+SerialMonitor monitor = new(port, delayMS);
+
+// Register CTRL+C
+Console.CancelKeyPress += delegate
+{
+    log.Info("Exiting");
+    monitor.Stop();
+};
+
+
+// Start reading monitor
+try
+{
+    monitor.Start();
+}
+catch (System.IO.IOException ex)
+{
+    log.Fatal(ex, "Could not start monitor");
+    return;
+}
+
+
+while (true) { }
---
+++ SerialMonitor.cs
@@ -0,0 +1,93 @@
+//
+
+using System.IO.Ports;
+using NLog;
+using System.Diagnostics;
+using System.Text;
+
+public class SerialMonitor
+{
+    private static readonly Logger log = LogManager.GetCurrentClassLogger();
+    private const int BAUDRATE = 9600;
+    private Stopwatch watch = new();
+    private readonly uint delayMS;
+    private SerialPort port;
+    private Timer timer;
+    
+
+    const uint DEFAULT_DELAY_MS = 10;
+
+    public SerialMonitor(string comPort, uint? delayMS = null)
+    {
+        if (delayMS != null)
+        {
+            this.delayMS = (uint)delayMS;
+        }
+        else
+        {
+            this.delayMS = DEFAULT_DELAY_MS;
+        }
+
+        // Initialize the comport
+        port = new(comPort);
+
+        //initilize timer
+        timer = new(readSerial, this, System.Threading.Timeout.Infinite, this.delayMS);
+    }
+
+    /// <summary>
+    /// Start the Montoring
+    /// </summary>
+    public void Start()
+    {
+        port.BaudRate = BAUDRATE;
+
+        port.Open();
+        // Start watch
+
+        watch.Start();
+        timer.Change(0, this.delayMS);
+    }
+
+    /// <summary>
+    /// Stop the monitor
+    /// </summary>
+    public void Stop()
+    {
+        watch.Stop();
+        port.Close();
+        timer.Change(System.Threading.Timeout.Infinite,
+            System.Threading.Timeout.Infinite);
+    }
+
+    /// <summary>
+    /// Periodically read the data from serial and print to nlog
+    /// </summary>
+    /// <param name="stateInfo"></param>
+    /// <exception cref="NullReferenceException"></exception>
+    private static void readSerial(Object? stateInfo)
+    {
+        SerialMonitor? monitor = stateInfo as SerialMonitor;
+        if (monitor == null) throw new NullReferenceException();
+
+        if (monitor.port.IsOpen == false)
+            return;
+
+        if (monitor.port.BytesToRead > 0)
+        {
+            StringBuilder output = new();
+            output.Append(monitor.watch.ElapsedMilliseconds);
+            output.Append(":");
+
+            for (int i = 0; i < monitor.port.BytesToRead; ++i)
+            {
+                byte b = (byte)monitor.port.ReadByte();
+                output.AppendFormat(" {0:2X}", b);
+            }
+            output.AppendLine();
+
+            // Print to nlog
+            log.Info(output.ToString());
+        }
+    }
+}
