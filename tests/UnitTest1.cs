4ae9e1211086b2b6b3c37cd059a743cef8117fbf
Andreas Kurzmann <81073076+stprograms@users.noreply.github.com>
2025-02-19 19:49:28 +0000

    17 - Introduced test project
    Added a testproject to the solution and created first dummy test

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace RS485_Monitor.tests;
+using System;
+using CommandLine;
+using NUnit.Framework;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
