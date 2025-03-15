417378f11347edea5517b3f4073fae815c29a1e2
Andreas Kurzmann <andreas.kurzmann@a1.net>
2025-03-15 18:54:52 +0100

    23 - Added TelegramImporter
    - Added TelegramImporter class and unit tests
    - Added optional leaveOpen argument to constructors

--- RS485 Monitor/src/Utils/Storage/TelegramExporter.cs
+++ RS485 Monitor/src/Utils/Storage/TelegramExporter.cs
@@ -5,24 +5,29 @@ namespace RS485_Monitor.Utils.Storage
     /// <summary>
     /// This class will export the telegrams into a binary format to a stream.
     /// </summary>
-    public class TelegramWriter :IDisposable
+    public class TelegramExporter :IDisposable
     {
         public const byte VERSION = 1;
         public const string IDENTIFIER = "RS485MONITOR";
-        public readonly byte[] MAGIC_NUMBER = Encoding.ASCII.GetBytes(IDENTIFIER);
+        public static readonly byte[] MAGIC_NUMBER = Encoding.ASCII.GetBytes(IDENTIFIER);
 
         private readonly BinaryWriter _writer;
 
-        public TelegramWriter(Stream stream)
+        /// <summary>
+        /// Create a new TelegramExporter from a stream.
+        /// </summary>
+        /// <param name="stream">stream to read from</param>
+        /// <param name="leaveOpen">leave the stream open after reading</param>
+        public TelegramExporter(Stream stream, bool leaveOpen = false)
         {
-            _writer = new BinaryWriter(stream);
+            _writer = new BinaryWriter(stream, new UTF8Encoding(), leaveOpen);
             WriteHeader();
         }
 
-        public TelegramWriter(string path)
+        public TelegramExporter(string path, bool leaveOpen = false)
         {
             FileStream stream = new(path, FileMode.Create);
-            _writer = new BinaryWriter(stream);
+            _writer = new BinaryWriter(stream, new UTF8Encoding(), leaveOpen);
             WriteHeader();
         }
 
--- tests/TelegramExporterTest.cs
+++ tests/TelegramExporterTest.cs
@@ -4,17 +4,17 @@ using NUnit.Framework;
 using System.Text;
 
 [TestFixture]
-public class TelegramWriterTest
+public class TelegramExporterTest
 {
 
     [Test]
     public void WriteHeaderTest()
     {
         MemoryStream stream = new();
-        using TelegramWriter writer = new(stream);
+        using TelegramExporter writer = new(stream);
 
         // Extract identifier
-        byte[] expected = Encoding.ASCII.GetBytes(TelegramWriter.IDENTIFIER);
+        byte[] expected = Encoding.ASCII.GetBytes(TelegramExporter.IDENTIFIER);
         byte[] actual = new byte[expected.Length];
 
         stream.Seek(0, SeekOrigin.Begin);
@@ -33,7 +33,7 @@ public class TelegramWriterTest
     public void CreateFileSuccess() {
         string path = Path.GetTempFileName();
 
-        using (TelegramWriter writer = new(path)) {
+        using (TelegramExporter writer = new(path)) {
             Assert.That(File.Exists(path), Is.True);
         }
 
@@ -41,12 +41,12 @@ public class TelegramWriterTest
 
             // Read the content of the file and check the header
             using (var reader = new BinaryReader(stream)) {
-                byte[] actual = new byte[TelegramWriter.IDENTIFIER.Length];
-                reader.Read(actual, 0, TelegramWriter.IDENTIFIER.Length);
+                byte[] actual = new byte[TelegramExporter.IDENTIFIER.Length];
+                reader.Read(actual, 0, TelegramExporter.IDENTIFIER.Length);
                 byte actual_version = reader.ReadByte();
 
-                byte[] expected = Encoding.ASCII.GetBytes(TelegramWriter.IDENTIFIER);
-                byte expected_version = TelegramWriter.VERSION;
+                byte[] expected = Encoding.ASCII.GetBytes(TelegramExporter.IDENTIFIER);
+                byte expected_version = TelegramExporter.VERSION;
 
                 Assert.That(actual, Is.EqualTo(expected));
                 Assert.That(actual_version, Is.EqualTo(expected_version));
@@ -61,7 +61,7 @@ public class TelegramWriterTest
     [Test]
     public void PushTelegrams() {
         MemoryStream stream = new();
-        using TelegramWriter writer = new(stream);
+        using TelegramExporter writer = new(stream);
 
 
         byte[] raw1 = [0xB6, 0x6B, 0xAA, 0x5A, 0x0A, 0x4D, 0x48, 0x17, 0x00, 0x00, 0x23, 0x00, 0x0B, 0x00, 0x00, 0x30, 0x0D];
@@ -74,7 +74,7 @@ public class TelegramWriterTest
         }
 
         // Jump over the header and start reading the telegrams
-        var headerlen = TelegramWriter.IDENTIFIER.Length + 1;
+        var headerlen = TelegramExporter.IDENTIFIER.Length + 1;
         stream.Seek(headerlen, SeekOrigin.Begin);
 
         using (var reader = new BinaryReader(stream)) {
